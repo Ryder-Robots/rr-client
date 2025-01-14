26f968ac5ec6cf3641ee0f61929f744a39a046ff
Aaron Spiteri <azzmosphere@gmail.com>
2025-01-14 20:48:35 +1100

    Removed mode change code, to resolve inconsistency

--- core/src/main/java/org/ryderrobot/drones/DroneSdkLd001.java
+++ core/src/main/java/org/ryderrobot/drones/DroneSdkLd001.java
@@ -6,43 +6,48 @@ import org.ryderrobot.models.protocols.rrp.*;
 public class DroneSdkLd001 extends AbstractDrone{
     final short EN_BACKWARD =  0b0101;
     final short EN_FORWARD =  0b1010;
+
+    final short EN_LEFT = 0b0110;
+    final short EN_RIGHT = 0b1001;
+
     final int OFFSET = 1000;
     final int MANFLIGHT_BMASK = 1 << 9;
+    boolean mode_sent = false;
 
 
+    // TODO: this is to be deprecated, for now just assume that once we have sent an event it is
+    // handled.
     @Override
-    public void handlerControllerEvent(ControllerRequest handler) {
+    public void handlerControllerEvent(ControllerRequest handler) throws RuntimeException {
         // check if mode is set here,
-        if ((getStatus().getFlag() &  MANFLIGHT_BMASK) != MANFLIGHT_BMASK) {
+        //if ((getStatus().getFlag() &  MANFLIGHT_BMASK) != MANFLIGHT_BMASK) {
+        if (!mode_sent) {
             RrpEvent<MspModePayload> modeEvent = new RrpEvent<>(
                 RrpCommands.MSP_MODE,
                 new MspModePayload(MspMode.CMODE_MANUAL_FLIGHT)
             );
             getSocketClient().send(modeEvent);
+            mode_sent = true;
             return;
         }
 
         short in;
         float motor1, motor2, motor3 = 0, motor4 = 0;
 
-        if (handler.getAxisYL() > 0) {
+        if (handler.getAxisYL() > 0.05) {
             in = EN_FORWARD;
-            if (handler.getAxisXL() > 0 || handler.getAxisXL() == 0) {
-                motor1 = handler.getAxisYL();
-                motor2 = handler.getAxisYL() - handler.getAxisXL();
-            } else {
-                motor1 = handler.getAxisYL() + handler.getAxisXL();
-                motor2 = handler.getAxisYL();
-            }
+            motor1 = handler.getAxisYL();
+            motor2 = handler.getAxisYL();
         } else {
             in = EN_BACKWARD;
-            if (handler.getAxisXL() > 0 || handler.getAxisXL() == 0) {
-                motor1 = handler.getAxisYL() * -1;
-                motor2 = (handler.getAxisYL() * -1) - handler.getAxisXL();
-            } else {
-                motor1 = (handler.getAxisYL() * -1) + handler.getAxisXL();
-                motor2 = handler.getAxisYL() * -1;
-            }
+            motor1 = handler.getAxisYL() * -1;
+            motor2 = handler.getAxisYL() * -1;
+        }
+
+        if (handler.getAxisXR() > 0.05) {
+            in = EN_RIGHT;
+        } else if (handler.getAxisXR() < -0.05) { //if (handler.getAxisXL() < 0) {
+            in = EN_LEFT;
         }
 
 
